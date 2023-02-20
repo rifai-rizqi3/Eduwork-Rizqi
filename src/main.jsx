bb1114881a57e8df1fd8ab6e7b4817732da14619
RIZQI NUR RIFA'I <84585203+rifai-rizqi3@users.noreply.github.com>
2023-02-20 20:26:37 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Navbar from './Navbar/index'
+import Navbar from './components/Navbar/index'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
